050cca0411446e7642136aa5afedc8535f32388d
hyeonwater <hs97kim@gmail.com>
2022-10-31 23:41:31 +0900

    first git

---
+++ lib/pages/header.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text('data')
+      ],
+    );
+  }
+}
